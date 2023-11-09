260347d03754c3421826ca36ae9928311d6d12f4
alex-sly <sly1521@mail.ru>
2023-11-09 21:55:45 +0500

    Задание lesson06

--- script.js
+++ script.js
@@ -1,96 +1,37 @@
 "use strict";
 
-let title;
-let screens;
-let screenPrice;
-let adaptive;
-let rollback = 10;
-let allServicePrices;
-let fullPrice;
-let servicePercentPrice;
-let service1;
-let service2;
-
 const isNumber = function (num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
-
-const asking = function () {
-  title = prompt("Как называется ваш проект?", "Калькулятор верстки");
-  screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
-
-  do {
-    screenPrice = prompt("Сколько будет стоить данная работа?");
-  } while (!isNumber(screenPrice));
-  screenPrice = +screenPrice;
-
-  adaptive = confirm("Нужен ли адаптив на сайте?");
-};
-
-const getAllServicePrices = function () {
-  let sum = 0;
-  let servicePrice;
-  for (let i = 0; i < 2; i++) {
-    if (i === 0) {
-      service1 = prompt("Какой дополнительный тип услуги нужен?", "Метрика");
-    } else {
-      service2 = prompt("Какой дополнительный тип услуги нужен?", "Отправка форм");
-    }
-
-    do {
-      servicePrice = prompt("Сколько это будет стоить?");
-    } while (!isNumber(servicePrice));
-    sum += +servicePrice;
+function inputNum(vopros) {
+  let num = prompt(vopros);
+  if (num === null) {
+    num = 0;
+  } else if (isNumber(num)) {
+    num = +num;
+  } else {
+    num = inputNum("Введи число!");
   }
-  return sum;
-};
-
-const showTipeOf = function (variable) {
-  console.log(variable, typeof variable);
-};
-
-function getFullPrice() {
-  return screenPrice + allServicePrices;
+  return num;
+}
+function start(x) {
+  console.log(x);
+  function step() {
+    let num = inputNum("Угадай число от 1 до 100");
+    console.log(num);
+    if (num === 0) {
+      alert("Игра окончена");
+    } else if (num > x) {
+      alert("Загаданное число меньше");
+      step();
+    } else if (num < x) {
+      alert("Загаданное число больше");
+      step();
+    } else {
+      alert("Поздравляю, Вы угадали!!!");
+    }
+  }
+  step();
 }
 
-// Итоговая стоимость - откат посреднику
-const getServicePercentPrices = function () {
-  return fullPrice - fullPrice * (rollback / 100);
-};
-
-const getTitle = function () {
-  return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
-};
-
-const getRollbackMessage = function (price) {
-  if (price >= 30000) {
-    return "Даем скидку в 10%";
-  } else if (price >= 15000 && price < 30000) {
-    return "Даем скидку в 5%";
-  } else if (price >= 0 && price < 15000) {
-    return "Скидка не предусмотрена";
-  } else {
-    return "Что то пошло не так";
-  }
-};
-
-asking();
-allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice();
-servicePercentPrice = getServicePercentPrices();
-title = getTitle();
-
-showTipeOf(title);
-showTipeOf(screenPrice);
-showTipeOf(adaptive);
-
-console.log("allServicePrices: ", allServicePrices);
-
-console.log(getRollbackMessage(fullPrice));
-// console.log(typeof title);
-// console.log(typeof screenPrice);
-// console.log(typeof adaptive);
-// console.log(screens.length);
-console.log("Стоимость-откат посреднику: ", servicePercentPrice);
-console.log("Стоимость верстки экранов:  ", screenPrice);
-console.log("Стоимость разработки сайта: ", fullPrice);
+start(Math.trunc(Math.random() * 100) + 1);
