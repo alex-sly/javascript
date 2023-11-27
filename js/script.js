820f1b4a284b7292daaf5b6d2c9f455ac29f20ce
alex-sly <sly1521@mail.ru>
2023-11-27 12:22:18 +0500

    Задание lesson17

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ class Worker extends Person {
 
 const render = function (workers) {
   tableList.innerHTML = "";
-  workers.forEach((item) => {
+  workers.forEach((item, index) => {
     const tr = document.createElement("tr");
     let td = document.createElement("td");
     td.textContent = item.name;
@@ -47,7 +47,7 @@ const render = function (workers) {
     td.textContent = item.children;
     tr.append(td);
     td = document.createElement("td");
-    td.textContent = item.skills;
+    td.textContent = item._skills;
     tr.append(td);
     td = document.createElement("td");
     td.textContent = item.organization;
@@ -59,11 +59,16 @@ const render = function (workers) {
     td.textContent = item.date;
     tr.append(td);
     td = document.createElement("td");
-    td.innerHTML = `<td><img src="./img/trash.png" alt="" /></td>`;
+    td.innerHTML = `<td><img class="table__del" src="./img/trash.png" /></td>`;
+    td.querySelector(".table__del").addEventListener("click", (event) => {
+      workers.splice(index, 1);
+      render(workers);
+    })
     tr.classList.add("table__item");
     tr.append(td);
     tableList.append(tr);
   });
+  localStorage.setItem("lesson17", JSON.stringify(workers));
 };
 
 const addWorker = function (worker) {
@@ -73,16 +78,24 @@ const addWorker = function (worker) {
 
 btn.addEventListener("click", (event) => {
   event.preventDefault();
-  const worker = new Worker(inputs[0].value, inputs[1].value, inputs[2].checked, inputs[4].value);
-  worker.skills = inputs[3].value;
-  worker.position = inputs[5].value;
-  worker.date = inputs[6].value;
-  console.log(worker);
-  addWorker(worker);
+  if (inputs[0].value) {
+    const worker = new Worker(inputs[0].value, inputs[1].value, inputs[2].checked, inputs[4].value);
+    worker.skills = inputs[3].value;
+    worker.position = inputs[5].value;
+    worker.date = inputs[6].value;
+    console.log(worker);
+    addWorker(worker);
+    inputs[0].value = "";
+  }
 });
+
+if (localStorage.getItem("lesson17")) {
+  workers = JSON.parse(localStorage.getItem("lesson17"));
+  render(workers);
+}
 
 // const worker1 = new Worker("Alex", 33, true, "Завод");
 // worker1.skills = "Слесарь";
 // worker1.position = worker1.skills[0];
-// worker1.date = "2023-11-15";
+// worker1.date = "2023-11-01";
 // console.log("worker1: ", worker1);
