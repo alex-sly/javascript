f1e1a491d1aadae5302bfae34040c8a9d93b7fd7
alex-sly <sly1521@mail.ru>
2023-11-29 22:06:41 +0500

    Задание lesson20

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,6 +3,7 @@ const menu = () => {
   const menu = document.querySelector("menu");
   const closeBtn = menu.querySelector(".close-btn");
   const menuItems = menu.querySelectorAll("ul>li>a");
+  const nextBtn = document.querySelector("a[href='#service-block']");
 
   const handleMenu = () => menu.classList.toggle("active-menu");
 
@@ -20,5 +21,13 @@ const menu = () => {
       });
     })
   );
+  nextBtn.addEventListener("click", (event) => {
+    const id = nextBtn.getAttribute("href");
+    event.preventDefault();
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
 };
 export default menu;
