b8576e26e292a84cb4349feb11b0fb587ffd02d1
alex-sly <sly1521@mail.ru>
2023-12-02 23:00:52 +0500

    Задание lesson21

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -10,6 +10,25 @@ const validate = () => {
     });
   });
   // Формы
+  const checkInput = (e, text) => {
+    let inp = e.target.value.match(/\S+/g);
+    let out = [];
+    if (inp) {
+      inp.forEach((word) => {
+        let source;
+        do {
+          source = word;
+          word = source.replace("--", "-");
+        } while (word !== source);
+        word = word.replace(/^-(.+)/, (str, $1) => $1).replace(/(.+)-$/, (str, $1) => $1);
+        if (text) {
+          word = word[0].toUpperCase() + word.slice(1).toLowerCase();
+        }
+        out.push(word);
+      });
+      e.target.value = out.join(" ");
+    }
+  };
   forms.forEach((form) => {
     const textInput = form.querySelector("input[type='text']");
     const emailInput = form.querySelector("input[type='email']");
@@ -17,12 +36,21 @@ const validate = () => {
     textInput.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/[^а-яА-Я\-\s]+/g, "");
     });
+    textInput.addEventListener("blur", (e) => {
+      checkInput(e, true);
+    });
     emailInput.type = "text";
     emailInput.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/[^\w@\-\.\!\~\*\']+/g, "");
     });
+    emailInput.addEventListener("blur", (e) => {
+      checkInput(e, false);
+    });
     telInput.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/[^\d\(\)\-]+/g, "");
+    });
+    telInput.addEventListener("blur", (e) => {
+      checkInput(e, false);
     });
   });
   // Ваше сообщение
