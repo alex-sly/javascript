666d79f3652a78d0465d85179e2658cec141126f
alex-sly <sly1521@mail.ru>
2023-12-04 22:08:51 +0500

    Задание lesson22

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import validate from "./modules/validate";
+import tabs from "./modules/tabs";
 
 timer("30 november 2023");
 menu();
 modal();
 validate();
+tabs();
