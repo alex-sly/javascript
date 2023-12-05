7e40b540fa99cc419eca7ee2a98b997d951437dd
alex-sly <sly1521@mail.ru>
2023-12-05 19:07:08 +0500

    Задание lesson23

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import validate from "./modules/validate";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer("30 november 2023");
 menu();
 modal();
 validate();
 tabs();
+slider();
