bc7781e61c73d070761a156c9a0dc48ab494e8bb
alex-sly <sly1521@mail.ru>
2023-12-05 19:00:41 +0500

    Задание lesson22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,5 +1,5 @@
 const menu = () => {
-  const menuBtn = document.querySelector(".menu");
+  // const menuBtn = document.querySelector(".menu");
   const menu = document.querySelector("menu");
   const closeBtn = menu.querySelector(".close-btn");
   const menuItems = menu.querySelectorAll("ul>li>a");
@@ -7,25 +7,58 @@ const menu = () => {
 
   const handleMenu = () => menu.classList.toggle("active-menu");
 
-  menuBtn.addEventListener("click", handleMenu);
-  closeBtn.addEventListener("click", handleMenu);
-
-  menuItems.forEach((menuItem) =>
-    menuItem.addEventListener("click", (event) => {
-      const id = menuItem.getAttribute("href");
-      event.preventDefault();
-      handleMenu();
-      document.querySelector(id).scrollIntoView({
-        behavior: "smooth",
-      });
-    })
-  );
-  nextBtn.addEventListener("click", (event) => {
-    const id = nextBtn.getAttribute("href");
-    event.preventDefault();
-    document.querySelector(id).scrollIntoView({
+  const scroll = (link) => {
+    document.querySelector(link.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
     });
+  };
+
+  document.addEventListener("click", (e) => {
+    if (menu.classList.contains("active-menu")) {
+      e.preventDefault();
+      if (e.target.closest(".active-menu")) {
+        if (e.target === closeBtn) {
+          handleMenu();
+          return;
+        }
+        menuItems.forEach((item) => {
+          if (e.target === item) {
+            handleMenu();
+            scroll(e.target);
+            return;
+          }
+        });
+      } else {
+        handleMenu();
+      }
+    } else if (e.target.closest(".menu")) {
+      handleMenu();
+    } else if (e.target.closest("a[href='#service-block']")) {
+      e.preventDefault();
+      scroll(nextBtn);
+    }
   });
+
+  // menuBtn.addEventListener("click", handleMenu);
+
+  // closeBtn.addEventListener("click", handleMenu);
+  // menuItems.forEach((menuItem) =>
+  //   menuItem.addEventListener("click", (event) => {
+  //     const id = menuItem.getAttribute("href");
+  //     event.preventDefault();
+  //     handleMenu();
+  //     document.querySelector(id).scrollIntoView({
+  //       behavior: "smooth",
+  //     });
+  //   })
+  // );
+
+  // nextBtn.addEventListener("click", (event) => {
+  //   const id = nextBtn.getAttribute("href");
+  //   event.preventDefault();
+  //   document.querySelector(id).scrollIntoView({
+  //     behavior: "smooth",
+  //   });
+  // });
 };
 export default menu;
